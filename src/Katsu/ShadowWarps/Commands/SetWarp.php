5681e7b3d72d2bd630a1cef4b3cf910344539123
Katsu -.- <145829403+Katsu-MC@users.noreply.github.com>
2024-01-15 18:21:02 -0800

    plugin owned

--- src/Katsu/ShadowWarps/Commands/SetWarp.php
+++ src/Katsu/ShadowWarps/Commands/SetWarp.php
@@ -7,11 +7,13 @@ namespace Katsu\ShadowWarps\Commands;
 use pocketmine\command\CommandSender;
 use pocketmine\command\Command;
 use pocketmine\player\Player;
+use pocketmine\plugin\PluginOwned;
+use pocketmine\plugin\Plugin;
 
 use Katsu\ShadowWarps\WarpDelay;
 use Katsu\ShadowWarps\API\WarpAPI;
 
-class SetWarp extends Command
+class SetWarp extends Command implements PluginOwned
 {
     public function __construct()
     {
@@ -20,6 +22,11 @@ class SetWarp extends Command
         if (isset($command[1])) $this->setDescription($command[1]);
         $this->setAliases(WarpDelay::getConfigValue("setwarp_aliases"));
         $this->setPermission("shadowwarps.cmd.setwarp");
+        $this->plugin = $plugin;
+    }
+
+    public function getOwningPlugin(): Plugin {
+        return $this->plugin;
     }
 
     public function execute(CommandSender $sender, string $commandLabel, array $args)
