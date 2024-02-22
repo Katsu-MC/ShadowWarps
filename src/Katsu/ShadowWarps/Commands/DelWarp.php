2ad0c2e6a07161284f439c1ea99a7b8eb1b2bb4b
Katsu -.- <145829403+Katsu-MC@users.noreply.github.com>
2024-02-22 05:34:46 -0800

    Small fixes

--- src/Katsu/ShadowWarps/Commands/DelWarp.php
+++ src/Katsu/ShadowWarps/Commands/DelWarp.php
@@ -15,7 +15,10 @@ use Katsu\ShadowWarps\WarpDelay;
 
 class DelWarp extends Command implements PluginOwned
 {
-    public function __construct()
+
+    private $plugin;
+
+    public function __construct(WarpDelay $plugin)
     {
         $command = explode(":", WarpDelay::getConfigValue("delwarp_cmd"));
         parent::__construct($command[0]);
