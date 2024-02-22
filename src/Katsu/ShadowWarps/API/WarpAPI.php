7fbc1a745d4d3608841036c98b438047eb3f2d46
Katsu -.- <145829403+Katsu-MC@users.noreply.github.com>
2024-02-22 12:11:03 -0800

    possible fix

--- src/Katsu/ShadowWarps/API/WarpAPI.php
+++ src/Katsu/ShadowWarps/API/WarpAPI.php
@@ -13,40 +13,45 @@ use Katsu\ShadowWarps\WarpDelay;
 
 class WarpAPI
 {
-    public static Config $data;
+    private Config $data;
 
     public function __construct()
     {
-        self::$data = new Config(WarpDelay::getInstance()->getDataFolder() . "WarpsData.json", Config::JSON);
+        $this->data = new Config(WarpDelay::getInstance()->getDataFolder() . "WarpsData.json", Config::JSON);
     }
 
-    public static function getAllWarps(): array
+    public function getAllWarps(): array
     {
         $warps = [];
-        foreach (self::$data->getAll() as $name => $pos) {
+        foreach ($this->data->getAll() as $name => $pos) {
             $warps[] = $name;
         }
         return $warps;
     }
 
-    public static function existWarp(string $name): bool
+    public function existWarp(string $name): bool
     {
-        return in_array($name, self::getAllWarps());
+        return in_array($name, $this->getAllWarps());
     }
 
-    public static function addWarp(Player $pos, string $name): void
+    public function addWarp(Player $pos, string $name): void
     {
-        self::$data->set($name, [$pos->getPosition()->getX(), $pos->getPosition()->getY(), $pos->getPosition()->getZ(), $pos->getWorld()->getDisplayName()]);
+        $this->data->set($name, [$pos->getPosition()->getX(), $pos->getPosition()->getY(), $pos->getPosition()->getZ(), $pos->getWorld()->getDisplayName()]);
     }
 
-    public static function getWarp(string $name): Position
+    public function getWarp(string $name): Position
     {
-        $pos = self::$data->get($name);
+        $pos = $this->data->get($name);
         return new Position($pos[0], $pos[1], $pos[2], Server::getInstance()->getWorldManager()->getWorldByName($pos[3]));
     }
 
-    public static function delWarp(string $name): void
+    public function delWarp(string $name): void
     {
-        self::$data->remove($name);
+        $this->data->remove($name);
+    }
+
+    public function save(): void
+    {
+        $this->data->save();
     }
 }
