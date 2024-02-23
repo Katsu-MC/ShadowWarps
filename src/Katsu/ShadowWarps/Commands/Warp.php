984cda95b96c7715c3325d5dfe46e3600d910caf
Katsu -.- <145829403+Katsu-MC@users.noreply.github.com>
2024-02-22 18:23:26 -0800

    Fixes

--- src/Katsu/ShadowWarps/Commands/Warp.php
+++ src/Katsu/ShadowWarps/Commands/Warp.php
@@ -9,8 +9,8 @@ use pocketmine\command\Command;
 use pocketmine\entity\effect\EffectInstance;
 use pocketmine\entity\effect\VanillaEffects;
 use pocketmine\player\Player;
-use pocketmine\plugin\Plugin;
 use pocketmine\plugin\PluginOwned;
+use pocketmine\plugin\Plugin;
 
 use Katsu\ShadowWarps\WarpDelay;
 use Katsu\ShadowWarps\Forms\WarpForms;
@@ -19,10 +19,10 @@ use Katsu\ShadowWarps\API\WarpAPI;
 
 class Warp extends Command implements PluginOwned
 {
-
     private $plugin;
+    private $warpAPI;
 
-    public function __construct(WarpDelay $plugin)
+    public function __construct(WarpDelay $plugin, WarpAPI $warpAPI)
     {
         $command = explode(":", WarpDelay::getConfigValue("warp_cmd"));
         parent::__construct($command[0]);
@@ -30,9 +30,11 @@ class Warp extends Command implements PluginOwned
         $this->setAliases(WarpDelay::getConfigValue("warp_aliases"));
         $this->setPermission("shadowwarps.cmd.warp");
         $this->plugin = $plugin;
+        $this->warpAPI = $warpAPI;
     }
 
-    public function getOwningPlugin(): Plugin {
+    public function getOwningPlugin(): Plugin
+    {
         return $this->plugin;
     }
 
@@ -41,9 +43,9 @@ class Warp extends Command implements PluginOwned
         if ($sender instanceof Player) {
             $command = explode(":", WarpDelay::getConfigValue("warp_cmd"));
             if ((isset($command[2])) and (WarpDelay::hasPermissionPlayer($sender, $command[2]))) return;
-            if ((isset($args[0])) and (WarpAPI::existWarp($args[0]))) {
+            if ((isset($args[0])) and ($this->warpAPI->existWarp($args[0]))) {
                 if (($sender->hasPermission("shadowwarps.cmd.warps"))) {
-                    $sender->teleport(WarpAPI::getWarp($args[0]));
+                    $sender->teleport($this->warpAPI->getWarp($args[0]));
                     $sender->sendMessage(WarpDelay::getConfigReplace("warp_msg_teleport"));
                 } else {
                     $sender->getEffects()->add(new EffectInstance(VanillaEffects::BLINDNESS(), 20 * (WarpDelay::getConfigValue("delay") + 2), 10));
@@ -55,7 +57,7 @@ class Warp extends Command implements PluginOwned
                     return;
                 }
 
-                $warps = implode(", ", WarpAPI::getAllWarps());
+                $warps = implode(", ", $this->warpAPI->getAllWarps());
                 $sender->sendMessage(WarpDelay::getConfigReplace("warp_msg_list", ["{warp}"], [$warps]));
             }
         }
